9989aa0b9cdfc643d3032fd6b720083d98dcc879
Gleryston Matos <glerystonmatos@yahoo.com.br>
2020-06-21 14:06:55 -0300

    Fontes iniciais do projeto.

---
+++ AutoMapperNetCore.Domain/Dto/UsuarioDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace AutoMapperNetCore.Domain.Dto
+{
+    public class UsuarioDto
+    {
+        public UsuarioDto(string nome)
+        {
+            Id = Guid.NewGuid();
+            Nome = nome;
+        }
+
+        public Guid Id { get; }
+
+        public string Nome { get; }
+    }
+}
---
+++ AutoMapperNetCore.Domain/Entities/Usuario.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace AutoMapperNetCore.Domain.Entities
+{
+    public class Usuario
+    {
+        public Usuario(string nome)
+        {
+            Id = Guid.NewGuid(); ;
+            Nome = nome;
+        }
+
+        public Guid Id { get; }
+
+        public string Nome { get; }
+    }
+}
